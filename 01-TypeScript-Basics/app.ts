764bf003b3a095b1ed038d119ca6ed13a318aa3f
dmelim <dmpmelim@gmail.com>
2022-08-30 22:34:49 +0100

    TypeScript basic types.

--- 01-TypeScript-Basics/app.ts
+++ 01-TypeScript-Basics/app.ts
@@ -1,1 +1,14 @@
-console.log('Your code goes here...');
+// Basic TS types.
+
+//TS numbers example, and how to state types
+
+function add(n1: number, n2: number): number {
+  return n1 + n2;
+}
+
+const number1 = 5;
+
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
